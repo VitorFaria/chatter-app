889a622c83fe8b8ab6a5711a55ec8346e7701c0e
VitorFaria <vitorfaria963@hotmail.com>
2025-05-30 06:21:08 -0300

    Create api dockerfile, install node packages and create initial config with mongodb

--- chatter-api/src/app.module.ts
+++ chatter-api/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { DatabaseModule } from './common/database/database.module';
+import * as Joi from 'joi';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    DatabaseModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
