319c82a39f723fcc995e65266e08ce03662cbf4b
VitorFaria <vitorfaria963@hotmail.com>
2025-06-05 17:33:34 -0300

    exports user service and import authModule to app module

--- chatter-api/src/users/users.module.ts
+++ chatter-api/src/users/users.module.ts
@@ -12,5 +12,6 @@ import { User, userSchema } from './entities/user.entity';
     ])
   ],
   providers: [UsersResolver, UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
