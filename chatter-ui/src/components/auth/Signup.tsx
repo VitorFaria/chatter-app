fec708493b762a69d0074c3a9f099c06fa9f9f44
VitorFaria <vitorfaria963@hotmail.com>
2025-06-09 19:37:52 -0300

    Add error prop on components, to capture the auth error validations and show it to the user

--- chatter-ui/src/components/auth/Signup.tsx
+++ chatter-ui/src/components/auth/Signup.tsx
@@ -2,20 +2,36 @@ import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
 import { useCreateUser } from "../../hooks/useCreateUser";
+import { useState } from "react";
+import { extractErrorMessage } from "../../utils/errors";
 
 const Signup = () => {
   const [createUser] = useCreateUser();
+  const [error, setError] = useState("");
 
   return (
-    <Auth submitLabel="Signup" onSubmit={async ({email, password}) => {
-      await createUser({
-        variables: {
-          createUserInput: {
-            email,
-            password,
-          },
-        },
-      })
+    <Auth 
+      submitLabel="Signup" 
+      error={error}
+      onSubmit={async ({email, password}) => {
+        try {
+          await createUser({
+            variables: {
+              createUserInput: {
+                email,
+                password,
+              },
+            },
+          })
+        } catch (err) {
+          const errorMessage = extractErrorMessage(err);
+
+          if (errorMessage) {
+            setError(errorMessage);
+            return;
+          }
+          setError("Something went wrong.Try again later.")
+        }
     }}>
       <Link to={'/login'} style={{alignSelf: 'center'}}>
         <MUILink>Login</MUILink>
