b62488c953a6a9ddf178edba107a11b04b58a68a
VitorFaria <vitorfaria963@hotmail.com>
2026-07-14 07:18:45 -0300

    Adds authenticated and unauthenticated routes, to control what the user can see

--- chatter-ui/src/components/header/Header.tsx
+++ chatter-ui/src/components/header/Header.tsx
@@ -6,20 +6,39 @@ import MobileNavigation from './mobile/MobileNavigation';
 import MobileBranding from './mobile/MobileBranding';
 import Navigation from './Navigation';
 import Settings from './Settings';
+import { useReactiveVar } from '@apollo/client';
+import { authenticatedVar } from '../../constants/authenticated';
+import { IPage } from '../../interfaces/page.interface';
 
-const pages: string[] = [];
+const pages: IPage[] = [
+  {
+    title: 'Home',
+    path: '/',
+  }
+];
+
+const unauthenticatedPages: IPage[] = [
+  {
+    title: 'Login',
+    path: '/login',
+  },
+  {
+    title: 'Signup',
+    path: '/signup',
+  }
+]
 
 const Header = () => {
+  const authenticated = useReactiveVar(authenticatedVar);
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Branding/>
-          <MobileNavigation pages={pages} />
+          <MobileNavigation pages={authenticated ? pages : unauthenticatedPages} />
           <MobileBranding/>
-          <Navigation pages={pages}/>
-          
-          <Settings/>
+          <Navigation pages={authenticated ? pages : unauthenticatedPages}/>
+          {authenticated && <Settings/>}
         </Toolbar>
       </Container>
     </AppBar>
