5a75d883ac5d0b9e2384b3274003ba25012279bb
VitorFaria <vitorfaria963@hotmail.com>
2026-07-15 05:05:07 -0300

    Creates snackbar component and call it inside main component to be rendered when it needs

---
+++ chatter-ui/src/components/snackbar/Snackbar.tsx
@@ -0,0 +1,41 @@
+import { useReactiveVar } from '@apollo/client';
+import {Snackbar as MUISnackbar, SnackbarCloseReason} from '@mui/material';
+import Alert from '@mui/material/Alert';
+import React from 'react';
+import { snackVar } from '../../constants/snack';
+
+const Snackbar = () => {
+  const snack = useReactiveVar(snackVar);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    snackVar(undefined);
+  };
+
+  return (
+    <>
+      {
+        snack && (
+          <MUISnackbar open={!!snack} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity={snack?.type}
+            variant="filled"
+            sx={{ width: '100%' }}
+          >
+            {snack?.message}
+          </Alert>
+        </MUISnackbar>
+        )
+      }
+    </>
+  );
+}
+
+export default Snackbar;
