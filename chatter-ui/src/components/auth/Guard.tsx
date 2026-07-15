1d9a7b9e1f7839dcefbf13eae389ff0d0a8d1d3c
VitorFaria <vitorfaria963@hotmail.com>
2026-07-15 05:06:05 -0300

    Adds useEffect hook to list for any network error when getting user logged data

--- chatter-ui/src/components/auth/Guard.tsx
+++ chatter-ui/src/components/auth/Guard.tsx
@@ -2,18 +2,25 @@ import { JSX, useEffect } from "react";
 import { useGetMe } from "../../hooks/useGetMe";
 import excludedRoutes from "../../constants/excluded-routes";
 import { authenticatedVar } from "../../constants/authenticated";
+import { UNKNOWN_ERROR_SNACK_MESSAGE } from "../../constants/errors";
+import { snackVar } from "../../constants/snack";
 
 interface IGuardProps {
   children: JSX.Element;
 }
 
 const Guard = ({children}: IGuardProps) => {
-  const { data: user } = useGetMe();
+  const { data: user, error } = useGetMe();
 
   useEffect(() => {
     if (user) authenticatedVar(true);
 
   }, [user]);
+
+  useEffect(() => {
+    if (error?.networkError) snackVar(UNKNOWN_ERROR_SNACK_MESSAGE);
+
+  }, [error]);
 
   return (
     <>
