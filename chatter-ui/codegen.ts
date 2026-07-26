92f02b7d76f5bf4d65f2416693a8a3d95d64c765
VitorFaria <vitorfaria963@hotmail.com>
2026-07-26 01:25:41 -0300

    Install and create command to execute codegen on watch mode

---
+++ chatter-ui/codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://api:3000/graphql',
+  documents: ['src/**/*.{ts,tsx}'],
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+    },
+  },
+};
+
+export default config;
