ac90cdb266372c3a8bf8b0592b65125040b417b8
VitorFaria <vitorfaria963@hotmail.com>
2026-07-30 05:38:20 -0300

    Change interface to get whole chat props and add a button to access detailed chat

--- chatter-ui/src/components/chat-list/chat-list-item/ChatListItem.tsx
+++ chatter-ui/src/components/chat-list/chat-list-item/ChatListItem.tsx
@@ -4,33 +4,38 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import { ListItemButton } from '@mui/material';
+import { ChatFragmentFragment as Chat } from '../../../gql/graphql';
+import Router from '../../Routes';
 
 interface IChatListProps {
-  name?: string | null;
+  chat: Chat
 }
 
-const ChatListItem = ({ name }: IChatListProps) => {
+const ChatListItem = ({ chat }: IChatListProps) => {
   return (
     <>
       <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary={name}
-          secondary={
-            <>
-              <Typography
-                component="span"
-                variant="body2"
-                sx={{ color: 'text.primary', display: 'inline' }}
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </>
-          }
-        />
+        <ListItemButton onClick={() => Router.navigate(`/chats/${chat._id}`)}>
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary={chat?.name}
+            secondary={
+              <>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  sx={{ color: 'text.primary', display: 'inline' }}
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </>
+            }
+          />
+        </ListItemButton>
       </ListItem>
       <Divider variant="inset" component="li" />
     </>
