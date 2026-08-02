9377f076f3758d423873afccec4c18cbe3285e13
VitorFaria <vitorfaria963@hotmail.com>
2026-08-02 07:49:41 -0300

    Call hooks for creation and retrieve message data, to work on chat page

--- chatter-ui/src/components/chat/Chat.tsx
+++ chatter-ui/src/components/chat/Chat.tsx
@@ -1,15 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useGetChat } from "../../hooks/useGetChat";
-import { Divider, IconButton, InputBase, Paper, Stack } from "@mui/material";
+import { Box, Divider, IconButton, InputBase, Paper, Stack } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import { useState } from "react";
+import { useCreateMessage } from "../../hooks/useCreateMessage";
+import { useGetMessages } from "../../hooks/useGetMessages";
 
 const Chat = () => {
   const params = useParams();
-  const { data } = useGetChat({ _id: params._id! })
+  const [message, setMessage] = useState("");
+  const chatId = params._id!
+  const { data: chat } = useGetChat({ _id: chatId })
+  const [createMessage] = useCreateMessage();
+  const { data: messages } = useGetMessages({ chatId });
 
   return (
     <Stack sx={{ height: '100%', justifyContent: 'space-between'}}>
-      <h1>{data?.chat.name}</h1>
+      <h1>{chat?.chat.name}</h1>
+      <Box>
+        {messages?.messages.map((message) => (
+          <p key={message._id}>{message.content}</p>
+        ))}
+      </Box>
       <Paper sx={{
         p: '2px 4px',
         display: 'flex',
@@ -17,9 +29,23 @@ const Chat = () => {
         alignItems: 'center',
         width: '100%'
       }}>
-        <InputBase sx={{ ml: 1, flex: 1, width: '100%'}} placeholder="Message"/>
+        <InputBase 
+          sx={{ ml: 1, flex: 1, width: '100%'}}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message"/>
         <Divider sx={{ height: 28, m: 0.5}} orientation="vertical"/>
-        <IconButton color="primary" sx={{ p: "10px"}}>
+        <IconButton 
+          color="primary" 
+          sx={{ p: "10px"}}
+          onClick={() => createMessage({
+            variables: {
+              createMessageInput: {
+                content: message,
+                chatId
+              }
+            }
+          })}
+        >
           <SendIcon/>
         </IconButton>
       </Paper>
