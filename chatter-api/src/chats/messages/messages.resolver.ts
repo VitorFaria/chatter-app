4961d727a7bdaa8b43efb051fed0717e484b23bb
VitorFaria <vitorfaria963@hotmail.com>
2026-08-11 12:33:19 -0300

    Set return types & refact methods of messages, to work with new schema & object structure

--- chatter-api/src/chats/messages/messages.resolver.ts
+++ chatter-api/src/chats/messages/messages.resolver.ts
@@ -1,21 +1,18 @@
 import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { Inject, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
 import { GetMessagesArgs } from './dto/get-messages.args';
-import { PUB_SUB } from 'src/common/constants/injection-tokens';
-import { PubSub } from 'graphql-subscriptions';
 import { MessageCreatedArgs } from './dto/message-created.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
   constructor(
-    private readonly messagesService: MessagesService,
-    @Inject(PUB_SUB) private readonly pubSub: PubSub
+    private readonly messagesService: MessagesService
   ) {}
 
   @Mutation(() => Message)
@@ -23,30 +20,29 @@ export class MessagesResolver {
   async createMessage(
     @Args('createMessageInput') createMessageInput: CreateMessageInput,
     @CurrentUser() user: TokenPayload
-  ) {
+  ): Promise<Message> {
     return this.messagesService.createMessage(createMessageInput, user._id);
   }
 
   @Query(() => [Message], { name: 'messages' })
   @UseGuards(GqlAuthGuard)
   async getMessages(
-    @Args() getMessagesArgs: GetMessagesArgs,
-    @CurrentUser() user: TokenPayload
-  ) {
-    return this.messagesService.getMessages(getMessagesArgs, user._id);
+    @Args() getMessagesArgs: GetMessagesArgs
+  ): Promise<Message[]> {
+    return this.messagesService.getMessages(getMessagesArgs);
   }
 
   @Subscription(() => Message, {
     filter: (payload, variables, context) => {
       const userId = context.req.user._id;
-      return payload.messageCreated.chatId === variables.chatId
-      && userId !== payload.messageCreated.userId;
+      const message: Message = payload.messageCreated;
+      return message.chatId === variables.chatId
+      && userId !== message.user._id.toHexString();
     }
   })
   messageCreated(
-    @Args() messageCreatedArgs: MessageCreatedArgs,
-    @CurrentUser() user: TokenPayload
+    @Args() messageCreatedArgs: MessageCreatedArgs
   ) {
-    return this.messagesService.messageCreated(messageCreatedArgs, user._id);
+    return this.messagesService.messageCreated(messageCreatedArgs);
   }
 }
