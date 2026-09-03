cb5bf3a0a53cee7e7000265d6da4c70ff5cce448
VitorFaria <vitorfaria963@hotmail.com>
2026-09-03 15:58:39 -0300

    Add extraFields prop to show username on signup

--- chatter-ui/src/components/auth/Auth.tsx
+++ chatter-ui/src/components/auth/Auth.tsx
@@ -7,10 +7,11 @@ interface IAuthProps {
   submitLabel: string
   onSubmit: (credentials: { email: string, password: string }) => Promise<void>
   children: React.ReactNode,
+  extraFields?: React.ReactNode[];
   error?: string;
 }
 
-const Auth = ({ submitLabel, onSubmit, children, error }: IAuthProps ) => {
+const Auth = ({ submitLabel, onSubmit, children, error, extraFields }: IAuthProps ) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { data } = useGetMe();
@@ -38,6 +39,7 @@ const Auth = ({ submitLabel, onSubmit, children, error }: IAuthProps ) => {
       error={!!error}
       helperText={error}
       />
+      {extraFields}
       <TextField 
       type="password" 
       label="Password" 
