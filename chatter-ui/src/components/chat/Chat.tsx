86bc3b6e544fe3753cf7e9269f1994e0005767f3
VitorFaria <vitorfaria963@hotmail.com>
2026-09-21 17:05:58 -0300

    Remove hook call from chat component

--- chatter-ui/src/components/chat/Chat.tsx
+++ chatter-ui/src/components/chat/Chat.tsx
@@ -5,7 +5,6 @@ import SendIcon from '@mui/icons-material/Send';
 import { useEffect, useRef, useState } from "react";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
 import { useGetMessages } from "../../hooks/useGetMessages";
-import { useMessageCreated } from "../../hooks/useMessageCreated";
 
 const Chat = () => {
   const params = useParams();
@@ -16,8 +15,6 @@ const Chat = () => {
   const { data: messages } = useGetMessages({ chatId });
   const divRef = useRef<HTMLDivElement | null>(null);
   const location = useLocation();
-
-  useMessageCreated({ chatId });
 
   const scrollToBottom = () => divRef.current?.scrollIntoView();
 
