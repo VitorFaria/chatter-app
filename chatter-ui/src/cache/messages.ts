8042e308ff7023e226e161aff48a7e9e4afca585
VitorFaria <vitorfaria963@hotmail.com>
2026-09-24 18:21:55 -0300

    Set pagination props on messages caching

--- chatter-ui/src/cache/messages.ts
+++ chatter-ui/src/cache/messages.ts
@@ -1,12 +1,15 @@
 import { ApolloCache } from "@apollo/client";
 import { MessageFragmentFragment as Message } from "../gql/graphql";
 import { getMessagesDocument } from "../hooks/useGetMessages";
+import { PAGE_SIZE } from "../constants/page-size";
 
 export const updateMessages = (cache: ApolloCache<any>, message: Message) => {
   const messageQueryOptions = {
     query: getMessagesDocument,
     variables: { 
-      chatId: message.chatId 
+      chatId: message.chatId,
+      skip: 0,
+      limit: PAGE_SIZE
     },
   };
   const messages = cache.readQuery({ ...messageQueryOptions});
